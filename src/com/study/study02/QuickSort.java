72e2be4aac40f2c5ff1dc34f0e747b0499b30fd8
dabaobei <2650669236@qq.com>
2022-11-22 18:55:01 +0800

    master commit

--- src/com/study/study02/QuickSort.java
+++ src/com/study/study02/QuickSort.java
@@ -4,6 +4,7 @@ public class QuickSort {
     public static void main(String[] args) {
         System.out.println("hello!");
         System.out.println("Hello!");
+        System.out.println("Hello!");
 
     }
 }
